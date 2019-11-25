2bb36f780fc99fa3c2e34b00eb6515644b9d3119
BumbleFlash <sudarshan97@gmail.com>
2019-11-25 18:17:00 -0500

    Added the APIs for the ROR table and checked the results with the postman.

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -1,12 +1,96 @@
 let mysql = require('mysql');
 let con = mysql.createPool({
     connectionLimit: 10,
-    host:'localhost',
-    username:'root',
-    database:'project4',
-    password:'pammas',
+    host: "localhost",
+    user: "root",
+    database: "project4",
+    password: "pammas",
 });
 
 exports.getSideEffect = function(req,res){
+    let drugname = req.body.drugname;
+    var sql = "select r.pt, r.primaryid, r.caseid, d.drugname from drug d inner join reaction r on r.primaryid = d.primaryid and r.caseid = d.caseid and d.drugname = '" + drugname + "';";
+    con.query(sql, function (err, row, fields) {
+        if (err)
+            res.json({
+                response: false
+            });
+        else
+            var result = {
+                response: true,
+                result: row
+            };
+        res.json(result);
+    });
+};
+exports.getDrugAndSideEffect = function (req, res) {
+    let drugname = req.body.drugname;
+    let pt = req.body.pt;
+    let sql = "select count(pt) count from reaction r inner join drug d on r.primaryid = d.primaryid and r.caseid = d.caseid and drugname = '" + drugname + "' and pt = '" + pt + "';";
+    con.query(sql, function (err, row, fields) {
+        if (err)
+            res.json({
+                response: false
+            });
+        else {
+            var result = {
+                response: true,
+                result: row
+            };
+            res.json(result);
+        }
+    });
+};
+exports.getDrugAndNoSideEffect = function (req, res) {
+    let drugname = req.body.drugname;
+    let pt = req.body.pt;
+    let sql = "select count(pt) count from reaction r inner join drug d on r.primaryid = d.primaryid and r.caseid = d.caseid and drugname = '" + drugname + "' and pt <> '" + pt + "';";
+    con.query(sql, function (err, row, fields) {
+        if (err)
+            res.json({
+                response: false
+            });
+        else {
+            var result = {
+                response: true,
+                result: row
+            };
+            res.json(result);
+        }
+    });
+};
+exports.getNoDrugAndSideEffect = function (req, res) {
+    let drugname = req.body.drugname;
+    let pt = req.body.pt;
+    let sql = "select count(pt) count from reaction r inner join drug d on r.primaryid = d.primaryid and r.caseid = d.caseid and drugname <> '" + drugname + "' and pt ='" + pt + "';";
+    con.query(sql, function (err, row, fields) {
+        if (err)
+            res.json({
+                response: false
+            });
+        else {
+            var result = {
+                response: true,
+                result: row
+            };
+            res.json(result);
+        }
+    });
+};
+exports.getNoDrugAndNoSideEffect = function (req, res) {
 
+    let sql = "select count(pt) count from reaction r inner join drug d where r.primaryid = d.primaryid and d.caseid = r.caseid;";
+    con.query(sql, function (err, row, fields) {
+        if (err)
+            res.json({
+                response: false
+            });
+        else {
+            var result = {
+                response: true,
+                result: row
+            };
+            res.json(result);
+        }
+    });
 };
--- routes/data.js
+++ routes/data.js
@@ -2,5 +2,9 @@ var express = require('express');
 var router = express.Router();
 var dataController = require('../controllers/dataController');
 
-//router.get('/getSideEffect', dataController.getSideEffect());
+router.post('/getSideEffect', dataController.getSideEffect);
+router.post('/getDSP', dataController.getDrugAndSideEffect);
+router.post('/getDNSP', dataController.getDrugAndNoSideEffect);
+router.post('/getNDSP', dataController.getNoDrugAndSideEffect);
+router.get('/getNDNSP', dataController.getNoDrugAndNoSideEffect);
 module.exports = router;
